98a1a0b6fe6806522e41a4c6cd4db73805d6f894
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-01-07 19:11:03 +0500

    refactor HTML structure for hero section; update Tailwind CSS configuration for custom fonts and background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
       inter: ["Inter", "system-ui"],
     },
     backgroundImage: {
-      hero: "url('/images/png/hero.png')",
+      "hero": "url('/images/png/hero.png')",
 
       "hero-pattern": "url('/images/png/hero-bg-color.png')",
       "footer-texture": "url('/img/footer-texture.png')",
